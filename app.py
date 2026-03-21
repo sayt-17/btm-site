58012db99c5a1e9bdb61c5ec377c8415fd0d995d
site_0 <anikjanpanjiyev@gmail.com>
2026-03-22 01:53:21 +0500

    fix port

--- app.py
+++ app.py
@@ -26,3 +26,8 @@ def home():
 
     # 🔥 ENG MUHIM QATOR
     return render_template("index.html", t=t)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
